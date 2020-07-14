e76c98fb14a8aa8b4737af3c4bafa98affaa7bd6
Lyle Franklin <lylejfranklin@gmail.com>
2020-07-14 18:19:08 -0400

    Add minimal debian example

--- debian-buster/build-minimal-arm64.sh
+++ debian-buster/build-minimal-arm64.sh
@@ -3,14 +3,14 @@
 set -eu -o pipefail
 
 original_project_dir="$(cd "$(dirname "$0")/.." && pwd)"
-output_dir="${original_project_dir}/ubuntu-focal/output-rpi3"
+output_dir="${original_project_dir}/debian-buster/output-minimal-arm64"
 
 tmpdir="$(mktemp -d /tmp/packer.XXXXX)"
 trap '{ rm -rf ${tmpdir}; }' EXIT
 
 rsync -l -r --delete --exclude 'output*' --exclude .git "${original_project_dir}/" "${tmpdir}"
-pushd "${tmpdir}/ubuntu-focal" > /dev/null
-  PACKER_LOG=1 packer build packer-rpi3.json
+pushd "${tmpdir}/debian-buster" > /dev/null
+  PACKER_LOG=1 packer build packer-minimal-arm64.json
 
   mkdir -p "$(basename "${output_dir}")"
   mv "$(basename "${output_dir}")"/* "${output_dir}/"
